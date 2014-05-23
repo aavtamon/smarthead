54611d3b97919a3fef02fac4d5dab25896cc59fd
aavtamon <anton.avtamonov@gmail.com>
2014-05-23 16:43:12 -0400

    supported rotation angle detection

--- HeadController/src/com/pisoft/headcontroller/head/HeadController.java
+++ HeadController/src/com/pisoft/headcontroller/head/HeadController.java
@@ -84,4 +84,9 @@ public class HeadController {
 			}
 		});
 	}
+	
+	@JavascriptInterface
+	public int getCurrentAngle() {
+		return motion.getCurrentAngle();
+	}
 }
--- HeadController/src/com/pisoft/headcontroller/head/HeadMotion.java
+++ HeadController/src/com/pisoft/headcontroller/head/HeadMotion.java
@@ -3,12 +3,23 @@ package com.pisoft.headcontroller.head;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.util.Log;
 
 public class HeadMotion {
+	private static final int NUM_OF_READINGS_TO_AVERAGE = 30;
+	
 	private boolean initialized;
+	
+	private List<Integer> angleReadings = new ArrayList<Integer>(NUM_OF_READINGS_TO_AVERAGE);
 	
 	public interface OnCompleteListener {
 		public void onComplete();
@@ -20,6 +31,53 @@ public class HeadMotion {
 	
 	
 	public HeadMotion(final Activity activity) {
+		SensorManager sm = (SensorManager)activity.getSystemService(Context.SENSOR_SERVICE);
+		
+		SensorEventListener sl = new SensorEventListener() {
+			private float[] accelerometerValues;
+			private float[] magneticValues;
+			
+			public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			}
+
+			public void onSensorChanged(SensorEvent event) {
+			    if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			    	accelerometerValues = event.values.clone();
+			    }
+			    if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+			    	magneticValues = event.values.clone();
+			    }
+			    
+			    if (accelerometerValues != null && magneticValues != null) {
+			        float R[] = new float[16];
+			        if (SensorManager.getRotationMatrix(R, null, accelerometerValues, magneticValues)) {
+			        	float screenR[] = new float[16];
+			        	SensorManager.remapCoordinateSystem(R, SensorManager.AXIS_Y, SensorManager.AXIS_Z, screenR);
+			        	
+			        	float orientation[] = new float[3];
+						
+						SensorManager.getOrientation(screenR, orientation);
+						long azimut = Math.round(Math.toDegrees(orientation[0]) * 2) / 2;
+						int calculatedAngle = (int)((azimut + 360) % 360);
+						
+						synchronized (angleReadings) {
+							if (angleReadings.size() == NUM_OF_READINGS_TO_AVERAGE) {
+								angleReadings.remove(0);
+							}
+							angleReadings.add(calculatedAngle);
+						}
+						
+						//long pitch = Math.round(Math.toDegrees(orientation[1]));
+						//long roll = Math.round(Math.toDegrees(orientation[2]));
+			        }
+			    }
+			}
+		};
+		
+		sm.registerListener(sl, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+		sm.registerListener(sl, sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_GAME);
+		
+		
 		sendCommandToMotionDriver("isReady", new NetworkOperationListener() {
 			public void onComplete(int status) {
 				initialized = status == 200;
@@ -45,6 +103,16 @@ public class HeadMotion {
 		});
 		
 		return true;
+	}
+	
+	public int getCurrentAngle() {
+		synchronized (angleReadings) {
+			int sum = 0;
+			for (int i = 0; i < angleReadings.size(); i++) {
+				sum += angleReadings.get(i);
+			}
+			return sum / angleReadings.size();
+		}
 	}
 	
 	
